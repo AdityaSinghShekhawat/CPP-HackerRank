a8be1b181e954ed876fe78e06522e683536df982
AdityaSinghShekhawat <75027191+AdityaSinghShekhawat@users.noreply.github.com>
2021-01-12 12:15:40 +0530

    init

---
+++ 03-Basic-Data-Types.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long int a;
+    long long int b;
+    char c;
+    float d;
+    long double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << fixed << setprecision(3) << d << endl
+         << fixed << setprecision(9) << e;
+    return 0;
+}
